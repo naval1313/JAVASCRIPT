3cdc358287155c36238df78d369fe4a9ec0dac8b
naval1313 <141544626+naval1313@users.noreply.github.com>
2024-12-16 13:20:47 +0530

    string added to javascript

--- java.js
+++ java.js
@@ -1,3 +1,60 @@
 function ansh(){
     console.log("naval sir")
 }
+
+
+// ----------string----------------------------
+
+
+// // USING FOR OF LOOP IN STRINGS.
+
+// // let header="template literals";
+// // let tags=["template strings","javascript","es6"];
+// // let text =`<h2>${header}</h2><ul>`;
+
+// // for(let x of tags){
+// //     text+= `<li>${x}</li>`;
+// // }
+
+// // text+= `</ul`;
+
+// // document.getElementById("container").innerHTML = text;
+
+// // CALLBACK: FUNCTION PASSED AS AN ARGUMENT IN ANOTHER FUNCTION IS CALLED Callback.
+// // when we pass an function as an callback we does not use () paranthesis EX: display( show );
+// // here show is a callback func but we bdoes not use () here.
+
+// // CALLBACK IN FOR EACH LOOP.
+// // FOR EACH IS AN ARRAY METHOD, WE USE FOR EACH LOOP IN ARRAY ONLY.
+// // FOR EACH IS A FUNCTION WHICH WE CALL.
+// // FOR EACH() METHOD CALLS A FUNCTION (A CALLBACK FUNC).
+
+// const numbers=[10,20,30,40];
+
+//  numbers.forEach(calculate);
+
+// function calculate(x){
+// console.log(x*10);
+// }
+// // here x is an value we pass to get the values of array,we can use any value instead of x.
+
+
+// // MATH METHODS IN JS.
+// // SYNTX. MATH.METHOD(NUMBER)
+
+// // 1. MATH.PI METHOD : IT GIVES US THE PI VALUE.
+// console.log(Math.PI);
+
+// // 2. MATH.ROUND
+// Mwth.round(45.45125);
+
+// // 3. MATH.CEIL : ROUNDS TO THE NEARESR HIGHER VALUE.
+// Math.ceil(45.234);
+
+// // 4. MATH.ROUND : ROUNDS TO THE LOWEST VALUE
+// Math.floor(35.67);
+
+// // 5. MATH.TRUNK : IT RETURNS ONLY AN INTIGER PART.
+// Math.trunk();
+
+// // JSTEST:
